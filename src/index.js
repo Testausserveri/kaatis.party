8b3d66d29a8755f17047b3db6b40ce7a076dce75
Antti Ellilä <antti@antti.codes>
2025-03-20 12:00:05 +0200

    update comment

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ client.on('ready', async () => {
 client.on('message', async (message) => {
   if (message.channel.id !== memeChannelId) return
 
-  // The attachments of messages that are prefixed with '#' are not processed
+  // The attachments of messages that are prefixed with '!' are not processed
   if (message.content.startsWith('!')) return
 
   const contentRows = message.content.split('\n')
